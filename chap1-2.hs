7d4b990fe99cdacd2b1bae475ea16d3edcfe2f84
Renato <renato.scaroni@gmail.com>
2016-09-26 13:35:30 -0300

    Commenting my first function

--- chap1-2.hs
+++ chap1-2.hs
@@ -1,3 +1,4 @@
+-- Fibonacci calculator using list comprehension and some list/tupples clever methods
 fib n = do
     let fibs = 0 : 1 : [a + b | (a,b) <- zip fibs (tail fibs)]
     let nfibs = take n fibs
