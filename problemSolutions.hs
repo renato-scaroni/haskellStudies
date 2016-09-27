f56720e3e336578fee0ee6fcc60ff583a7577b34
scaroni <renato.scaroni@gmail.com>
2016-09-27 11:09:39 +0000

    correcting problem solution

--- problemSolutions.hs
+++ problemSolutions.hs
@@ -1,8 +1,19 @@
---wierd n
---    | mod n 2 == 1 = "wierd"
---    | elem n [x | x <- [2..5]] = "not wierd"
---    | elem n [x | x <- [6..20]] = "wierd"
---    | n > 20 = "not wierd"
+-- Problem: https://www.hackerrank.com/challenges/30-conditional-statements?h_r=email&unlock_token=0ffa35ffca0a3dc4214db15a493ad2b4d4da541c&utm_campaign=30_days_of_code_continuous&utm_medium=email&utm_source=daily_reminder
+import System.IO
+
+wierd n
+    | mod n 2 == 1 = "Wierd"
+    | elem n [x | x <- [2..5]] = "Not Wierd"
+    | elem n [x | x <- [6..20]] = "Wierd"
+    | n > 20 = "Not Wierd"
+
+main :: IO ()
+main = do
+    temp <- getLine
+    let n = read temp :: Int
+    putStrLn (wierd n)
+
+
 
 --getMultipleLines :: Int -> IO [String]
 --getMultipleLines n
@@ -12,9 +23,3 @@
 --        xs <- getMultipleLines (n-1)    
 --        let ret = (x:xs)    
 --        return ret          
-
---getInput = do
---    temp <- getLine
---    let n = read temp :: Int
---    n
---    --wierd n
